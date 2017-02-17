a542299af39ad22d19d6afd4c1020a62be227e6a
Alejandro U. Alvarez <aluralma@gmail.com>
2017-02-17 16:38:21 +0000

    restructure project, add --local setting

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
  * Ultimate Tic Tac Toe Algorithm Battle - Game Server
  */
 
+const localGame = require('./lib/local');
 
-console.info('Ultimate TTT Server');
+function server(options) {
+  console.info("+-----------------------------------+");
+  console.info("|   Ultimate TTT Algorithm Battle   |");
+  console.info("+-----------------------------------+");
+
+  if (options.local) {
+    localGame(options);
+  } else if (options.a || options.b) {
+    console.error('Error: Player files may only be specified for local games (use --local with -a and -b)');
+    return;
+  }
+}
+
+module.exports = server;
