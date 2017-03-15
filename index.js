ab40a3837520f39e12d2dec86406b032ffa12012
Alejandro U. Alvarez <aluralma@gmail.com>
2017-03-15 12:00:49 +0000

    add app.json for heroku deployments

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const localGame = require('./lib/local');
 const onlineGame = require('./lib/online');
 
 function server(options) {
+  // env overrides
+  options.port = process.env.TTT_PORT || options.port;
+  options.games = process.env.TTT_GAMES || options.games;
+  options.timeout = process.env.TTT_TIMEOUT || options.timeout;
+
   if (options.local) {
     localGame(options);
   } else if (options.a || options.b) {
