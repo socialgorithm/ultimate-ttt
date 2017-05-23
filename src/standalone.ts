582fb4198b7b1fff20e49295566041cb26e4247c
Alejandro U. Alvarez <aluralma@gmail.com>
2017-05-23 12:07:09 +0100

    add the standalone version

---
+++ src/standalone.ts
@@ -0,0 +1,13 @@
+/**
+ * Ultimate TTT Server - Standalone option
+ *
+ * Start a server with the default options. You can override some defaults with the following env vars:
+ *   - HOST Server host
+ *   - PORT Server port
+ *   - TTT_GAMES Number of games per session between users
+ *   - TTT_TIMEOUT Timeout per game
+ *
+ * Take a look at src/lib/input.js for the whole list of options available.
+ */
+import OnlineServer from './lib/OnlineServer';
+new OnlineServer({});
