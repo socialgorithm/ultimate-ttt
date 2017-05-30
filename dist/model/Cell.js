ef6748cd52c06d9a022ced4adc7243a330b4e536
Alejandro U. Alvarez <aluralma@gmail.com>
2017-05-30 11:32:41 +0100

    use UNPLAYED constant as initial value for cells

--- dist/model/Cell.js
+++ dist/model/Cell.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var constants_1 = require("./constants");
 var Cell = (function () {
     function Cell(coordinates, player, subBoardIndex, mainIndex) {
         if (coordinates === void 0) { coordinates = [0, 0]; }
-        if (player === void 0) { player = -1; }
+        if (player === void 0) { player = constants_1.UNPLAYED; }
         if (subBoardIndex === void 0) { subBoardIndex = -1; }
         if (mainIndex === void 0) { mainIndex = -1; }
         this.coordinates = coordinates;
@@ -15,7 +16,7 @@ var Cell = (function () {
         return new Cell(this.coordinates, this.player, this.subBoardIndex, this.mainIndex);
     };
     Cell.prototype.isPlayed = function () {
-        return this.player > -1;
+        return this.player > constants_1.RESULT_TIE;
     };
     return Cell;
 }());
--- src/model/Cell.ts
+++ src/model/Cell.ts
@@ -1,17 +1,17 @@
-import {PlayerNumber, PlayerOrTie, UNPLAYED, Coord} from "./constants";
+import {PlayerOrTie, UNPLAYED, Coord, RESULT_TIE} from "./constants";
 /**
  * Definition of each Cell on a SubBoard
  */
 export default class Cell {
 
-  constructor(public coordinates : Coord = [0, 0], public player: PlayerOrTie = -1, public subBoardIndex: number = -1,  public mainIndex: number = -1) { }
+  constructor(public coordinates : Coord = [0, 0], public player: PlayerOrTie = UNPLAYED, public subBoardIndex: number = -1,  public mainIndex: number = -1) { }
 
   copy() {
     return new Cell(this.coordinates, this.player, this.subBoardIndex, this.mainIndex);
   }
 
   isPlayed() {
-    return this.player > -1;
+    return this.player > RESULT_TIE;
   }
 
 }
