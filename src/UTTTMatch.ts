fda4232092f57c4be0c4508162a0a66c25c85e22
unknown <joffat@users.noreply.github.com>
2019-10-15 00:50:20 +0100

    log

--- src/UTTTMatch.ts
+++ src/UTTTMatch.ts
@@ -11,6 +11,7 @@ export default class UTTTMatch implements IMatch {
   private missingPlayers: Player[] = [];
 
   constructor(public options: MatchOptions, public players: Player[], private outputChannel: MatchOutputChannel) {
+    console.log(options);
     this.missingPlayers.push(...players);
     // Start a timeout for player connects
     setTimeout(() => {
