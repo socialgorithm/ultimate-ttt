2003f27d0998fed1d68fea777d221b12ea557cd2
Bharat Reddy <me@bharatreddy.com>
2021-09-02 02:02:23 +0100

    Remove server dist folder

--- packages/server/dist/Server.d.ts
+++
@@ -1,6 +0,0 @@
-import { ServerOptions } from "./cli/options";
-export default class Server {
-    private gameServer;
-    constructor(options: ServerOptions);
-    private newMatchFunction;
-}
--- packages/server/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var options_1 = require("./cli/options");
-var Server_1 = require("./Server");
-var server = new Server_1["default"](options_1.DEFAULT_OPTIONS);
-//# sourceMappingURL=index.js.map
